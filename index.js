67d547d10ac2bcfd07bf91bad8f06a7a8acc8e26
Devin Jaggernauth <neexis@gmail.com>
2023-11-09 20:45:52 -0400

    update port

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const {spawn} = require('child_process');
-const app = express()
-const PORT = 3000
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+const PORT = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
- 
- let dataToSend;
+  let dataToSend;
 
- const pythonProcess = spawn('python3', ['scripts/ml.py']);
- // collect data from script
- pythonProcess.stdout.on('data', function (data) {
-  dataToSend = data.toString();
- });
- 
- // close stream from child process
- pythonProcess.on('exit', (code) => {
- console.log(`Data sent successfully`)
- res.send(dataToSend)
- });
- 
-})
-app.listen(PORT, () => console.log(`🚀 Listening on Port 
-${PORT}!`))
+  const pythonProcess = spawn('python3', ['scripts/ml.py']);
+  // collect data from script
+  pythonProcess.stdout.on('data', function (data) {
+    dataToSend = data.toString();
+  });
+
+  // close stream from child process
+  pythonProcess.on('exit', (code) => {
+    console.log(`Data sent successfully`);
+    res.send(dataToSend);
+  });
+});
+app.listen(PORT, () =>
+  console.log(`🚀 Listening on Port 
+${PORT}!`)
+);
